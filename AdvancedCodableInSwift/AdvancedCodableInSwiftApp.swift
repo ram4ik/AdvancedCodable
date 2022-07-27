8e91ba2b49bb50ab3b78e569155484a7de6e9dfc
Ramil <ram4ik@hotmail.com>
2022-07-27 18:28:38 +0300

    Initial Commit

---
+++ AdvancedCodableInSwift/AdvancedCodableInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCodableInSwiftApp.swift
+//  AdvancedCodableInSwift
+//
+//  Created by Ramill Ibragimov on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCodableInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
